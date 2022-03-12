99648106cf7bc55f5bbf7ff79541d05c4ed2076e
Tushar <gtusharb49@gmail.com>
2022-03-13 00:45:43 +0530

    added and styled  header-component to our page without sign-in button

--- src/App.js
+++ src/App.js
@@ -3,34 +3,18 @@ import { Route } from "react-router-dom";
 import "./App.css";
 import Homepage from "./components/pages/homepage/homepage.component";
 import ShopPage from "./components/pages/homepage/shop/shop.component";
-
-const Hatspage = (props) => {
-  console.log(props);
-  return (
-    <div>
-      <h1>Hats Page </h1>
-    </div>
-  );
-};
-
-const Specifichatpage = (props) => {
-  return <h1> This is hat number {props.match.params.hatsID} </h1>;
-};
+import { Switch } from "react-router-dom";
+import Header from "./components/header/header.component";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Route exact path={"/"} component={Homepage} />
-        <Route exact path={"/hats"} component={Hatspage} />
-        <Route path={"/hats/:hatsID"} component={Specifichatpage} />
-        <Route path={"/shop"} component={ShopPage} />
-
-        <Route exact path={"/blog.hot/specific/hats"} component={Hatspage} />
-        <Route
-          path={"/blog.hot/specific/hats/:hatsID"}
-          component={Specifichatpage}
-        />
+        <Header />
+        <Switch>
+          <Route exact path={"/"} component={Homepage} />
+          <Route path={"/shop"} component={ShopPage} />
+        </Switch>
       </div>
     );
   }
